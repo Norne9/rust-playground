0979a0928114964bd081ff49fedbfb3127854ec2
Norne9 <aleksey191295@gmail.com>
2021-03-10 05:48:27 +0000

    Guess the number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess number app!");
+
+    let secret: u8 = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Enter number: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u8 = match guess.trim().parse() {
+            Err(_) => {
+                println!("You must enter a number!");
+                continue;
+            }
+            Ok(num) => num,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
