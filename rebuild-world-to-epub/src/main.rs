b02d48174212266a720c5018b690d11e7a54b8a9
Norne9 <aleksey191295@gmail.com>
2021-03-12 05:31:58 +0000

    Parser works

--- rebuild-world-to-epub/src/main.rs
+++ rebuild-world-to-epub/src/main.rs
@@ -1,91 +1,12 @@
-use scraper::{Html, Selector};
-
-struct Chapter {
-    name: String,
-    link: String,
-}
-type Index = Vec<Chapter>;
-struct Page {
-    title: String,
-    content: Vec<String>,
-}
+mod parser;
 
 fn main() {
-    let pages = get_index();
+    let parser = parser::BookParser::new();
+    let pages = parser.get_index();
     /*for page in pages.iter() {
         println!("{} - {}", page.name, page.link);
     }*/
     let page = &pages[0];
-    println!("{:?}", get_page(&page.link));
-}
-
-// https://www.divinedaolibrary.com/rebuild-world-chapter-1-akira-and-alpha/
-fn get_page(link: &str) -> Vec<String> {
-    let page = get_url(link);
-    let document = Html::parse_document(&page);
-
-    let title_selector = Selector::parse("h1.entry-title").unwrap();
-    let text_selector = Selector::parse("div.entry-content").unwrap();
-    let p_selector = Selector::parse("p").unwrap();
-
-    let content = document.select(&text_selector).next().unwrap();
-    let mut result: Vec<String> = Vec::new();
-
-    let stop_wrods = [
-        "\u{a0}",
-        "Index ",
-        "Translator: ",
-        "Editor",
-        "Proofreader: ",
-    ];
-
-    for item in content.select(&p_selector) {
-        let mut result_line = String::new();
-        for line in item.text() {
-            if stop_wrods.contains(&line) {
-                break;
-            }
-            result_line.push_str(line);
-        }
-
-        if !result_line.is_empty() {
-            result.push(result_line)
-        }
-    }
-
-    result
-}
-
-// https://www.divinedaolibrary.com/rebuild-world/
-fn get_index() -> Index {
-    let mut result: Index = Vec::new();
-
-    let index_page = get_url("https://www.divinedaolibrary.com/rebuild-world/");
-    let document = Html::parse_document(&index_page);
-
-    let chpaters_selector =
-        Selector::parse(r#"a[style="color: #ff9900; --darkreader-inline-color: #ffa31a;"]"#)
-            .unwrap();
-
-    for element in document.select(&chpaters_selector) {
-        let name = element.inner_html();
-        let link = String::from(element.value().attr("href").unwrap());
-        result.push(Chapter { name, link });
-    }
-
-    result
-}
-
-fn get_url(url: &str) -> String {
-    fn try_get_page(url: &str) -> Result<String, Box<dyn std::error::Error>> {
-        Ok(reqwest::blocking::get(url)?.text()?)
-    }
-    let mut last_err: Box<dyn std::error::Error> = Box::new(std::fmt::Error::default());
-    for _ in 0..3 {
-        match try_get_page(url) {
-            Ok(text) => return text,
-            Err(err) => last_err = err,
-        }
-    }
-    panic!("Failed to get page {}\nError: {}", url, last_err);
+    let page = parser.get_page(&page.link);
+    println!("{:?}", page);
 }
---
+++ rebuild-world-to-epub/src/parser.rs
@@ -0,0 +1,104 @@
+use scraper::{Html, Selector};
+
+pub struct BookParser {
+    title_selector: Selector,
+    text_selector: Selector,
+    p_selector: Selector,
+    chpaters_selector: Selector,
+    client: reqwest::blocking::Client,
+    stop_words: Vec<&'static str>,
+}
+pub struct Chapter {
+    pub name: String,
+    pub link: String,
+}
+pub type Index = Vec<Chapter>;
+#[derive(Debug)]
+pub struct Page {
+    pub title: String,
+    pub content: Vec<String>,
+}
+
+impl BookParser {
+    pub fn new() -> BookParser {
+        BookParser {
+            title_selector: Selector::parse("h1.entry-title").unwrap(),
+            text_selector: Selector::parse("div.entry-content").unwrap(),
+            p_selector: Selector::parse("p").unwrap(),
+            chpaters_selector: Selector::parse(
+                r#"a[style="color: #ff9900; --darkreader-inline-color: #ffa31a;"]"#,
+            )
+            .unwrap(),
+            client: reqwest::blocking::Client::new(),
+            stop_words: vec![
+                "\u{a0}",
+                "Index ",
+                "Translator: ",
+                "Editor",
+                "Proofreader: ",
+            ],
+        }
+    }
+
+    // https://www.divinedaolibrary.com/rebuild-world-chapter-1-akira-and-alpha/
+    pub fn get_page(&self, link: &str) -> Page {
+        let page = self.get_url(link);
+        let document = Html::parse_document(&page);
+
+        let title = document.select(&self.title_selector).next().unwrap();
+        let content = document.select(&self.text_selector).next().unwrap();
+
+        let mut lines = Vec::<String>::new();
+        for item in content.select(&self.p_selector) {
+            let mut result_line = String::new();
+            for line in item.text() {
+                if self.stop_words.contains(&line) {
+                    break;
+                }
+                result_line.push_str(line);
+            }
+
+            if !result_line.is_empty() {
+                lines.push(result_line)
+            }
+        }
+
+        Page {
+            title: title.inner_html(),
+            content: lines,
+        }
+    }
+
+    // https://www.divinedaolibrary.com/rebuild-world/
+    pub fn get_index(&self) -> Index {
+        let mut result: Index = Vec::new();
+
+        let index_page = self.get_url("https://www.divinedaolibrary.com/rebuild-world/");
+        let document = Html::parse_document(&index_page);
+
+        for element in document.select(&self.chpaters_selector) {
+            let name = element.inner_html();
+            let link = String::from(element.value().attr("href").unwrap());
+            result.push(Chapter { name, link });
+        }
+
+        result
+    }
+
+    fn get_url(&self, url: &str) -> String {
+        fn try_get_page(
+            client: &reqwest::blocking::Client,
+            url: &str,
+        ) -> Result<String, Box<dyn std::error::Error>> {
+            Ok(client.get(url).send()?.text()?)
+        }
+        let mut last_err: Box<dyn std::error::Error> = Box::new(std::fmt::Error::default());
+        for _ in 0..3 {
+            match try_get_page(&self.client, url) {
+                Ok(text) => return text,
+                Err(err) => last_err = err,
+            }
+        }
+        panic!("Failed to get page {}\nError: {}", url, last_err);
+    }
+}
